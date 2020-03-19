7978bc5770fc1be2864d04039cf90e1e4de33df7
Jakob Sandgren <jakob.sandgren@inspirationcompany.se>
2020-03-19 19:32:39 +0100

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
